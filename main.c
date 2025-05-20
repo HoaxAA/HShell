2025d660436c269f3a536c8095be29c2d8b4074d
Hoax <149868997+HoaxAA@users.noreply.github.com>
2025-05-20 23:49:15 +0330

    Add Get current working directory
    I added the option to print your working directory with
    getcwd() system call.
    and also the exit mechanism for exiting my shell
    
    Signed-off-by:HoaxAA

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char stringTokenizer(char*);
+char pwd();
+
+int
+main()
+{
+    char* input = malloc(256);
+
+    while (strcmp(input, "exit") != 0) {
+        scanf("%255s", input);
+        if (strcmp(input, "pwd") == 0) {
+            char* location = malloc(256);
+            getcwd(location, 256);
+            printf("%s\n", location);
+            free(location);
+        }
+    }
+}
